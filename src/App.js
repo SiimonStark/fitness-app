748fd80810d16ccfbdd969b3e17db2502045ae6a
siimonstark <siimonstark@gmail.com>
2022-06-13 12:41:34 -0700

    Establish Browser Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 import './App.css';
 
 const App = () => {
     return (
-        <div>app</div>
+        <Box width="400px" sx={{ width: { x1: '1488px' } }} m='auto'>
+            <NavBar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/exercise/:id' element={<ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </Box>
     )
 }
 
